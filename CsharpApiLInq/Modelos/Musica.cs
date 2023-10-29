942a39ebeb89f4fbdc06c513ce0f96a9e8920ae3
Diele <dybs1520@gmail.com>
2023-10-29 19:32:42 -0300

    musica

--- CsharpApiLInq/Modelos/Musica.cs
+++ CsharpApiLInq/Modelos/Musica.cs
@@ -1,14 +1,24 @@
-﻿
+﻿using System.Security.Principal;
+using System.Text.Json.Serialization;
 
 namespace CsharpApiLInq.Modelos;
 
 internal class Musica
 {
+    [JsonPropertyName("song")]//atributo que representa um metadado especifico para a propiedade Nome 
+    public string Nome { get; set; }
+    [JsonPropertyName("artist")]
+    public string Artista { get; set; }
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set;}// o "?" é que pode ser nula 
 
+    public void ExibirDetalhesDamusica()
+    {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Musica: {Nome}");
+        Console.WriteLine($"Duração em segundos: {Duracao /1000}");
+        Console.WriteLine($"Genero musical: {Genero}");
+    }
 }
-
-#region ANOTAÇÕES 
-/*
- 
- */
-#endregion
--- CsharpApiLInq/Program.cs
+++ CsharpApiLInq/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http.Json;
+﻿using CsharpApiLInq.Modelos;
+using System.Net.Http.Json;
+using System.Text.Json;
 //recurso para fazer requisições, não usamos o ":" pois vamos usar esse recurso so dentro do using //quando a chave fecha, liberamos o recurso objeto client 
 using (HttpClient client = new HttpClient())
 {
@@ -6,10 +8,17 @@ using (HttpClient client = new HttpClient())
     { 
     string resposta =await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.jso");
     Console.WriteLine(resposta);
+        #region deserialização
+        //Preciso ACESSAR as propriedades que estão na classe Musica,
+        //mas nesse caso estao em json e para isso eu faço uma conversão de json para
+        //C# e essa conversão é a deserialização
+        #endregion
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta);// vai pegar o o meu jso que está na string RESPOSTA e ai converter ele (deserialize) para uma lista do tipo da classe MUSICA e joar para a var musicas 
+        musicas[10].ExibirDetalhesDamusica();
     }
     catch (Exception ex) 
     { Console.WriteLine($"Temos um problema: {ex.Message}"); }
-    
+     //pegar o json para apontar para a classe musica 
 }
 
 
@@ -28,8 +37,9 @@ ele cai NA CAIXA de aviso/exceção. ~~Isso é o try(tentativa) catch(caixa)~~
 ermitindo capturar e tratar as exceções que ocorrem.Sem o try-catch, o programa poderia travar e mostrar mensagens 
 de erro confusas para o usuário,deixando o programa em um estado ruim.
  */
-/*DESERIALIZAR
- 
+/*SERIALIZAR/DESERIALIZAR
+    SERIALIZAR= pega um objeto e tansforma num fluxo de bites 
+    DESERIALIZAR= pega o fluxo de de bytes e tansforma em um objeto 
  
  */
 #endregion
