0fc888c3621b95802a240ae76c20b24962fddd9d
Diele <dybs1520@gmail.com>
2023-10-29 17:15:30 -0300

    Criei nova classe Musica: -fiz as propriedades  com atributos JsonPropertyName para relacionar com Api e pegar so aqueles dados

--- CsharpApiLInq/Modelos/Musca.cs
+++ CsharpApiLInq/Modelos/Musca.cs
@@ -1,6 +1,25 @@
-﻿namespace CsharpApiLInq.Modelos;
+﻿using System.Security.Principal;
+using System.Text.Json.Serialization;
+
+namespace CsharpApiLInq.Modelos;
 
 internal class Musca
 {
+    [JsonPropertyName("song")]//atributo que representa um metadado especifico para a propiedade Nome 
+    public string Nome { get; set; }
+    [JsonPropertyName("artist")]
+    public string Artista { get; set; }
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set;}// o "?" é que pode ser nula 
+
+    public void ExibirDetalhesDamusica()
+    {
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Musica: {Nome}");
+        Console.WriteLine($"Duração em segundos: {Duracao /1000}");
+        Console.WriteLine($"Genero musical: {Genero}");
+    }
 
 }
