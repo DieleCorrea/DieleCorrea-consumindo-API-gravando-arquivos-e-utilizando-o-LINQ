d80642cec9a1cbfa67e9e15df095a988135046c5
Diele <dybs1520@gmail.com>
2023-11-05 17:11:51 -0300

    chamei o linq na Program.cs

--- CsharpApiLInq/Program.cs
+++ CsharpApiLInq/Program.cs
@@ -1,4 +1,6 @@
-﻿using CsharpApiLInq.Modelos;
+﻿using CsharpApiLInq;
+using CsharpApiLInq.Filtros;
+using CsharpApiLInq.Modelos;
 using System.Text.Json;
 //recurso para fazer requisições, não usamos o ":" pois vamos usar esse recurso so dentro do using //quando a chave fecha, liberamos o recurso objeto client 
 using (HttpClient client = new HttpClient())
@@ -14,6 +16,7 @@ using (HttpClient client = new HttpClient())
         //C# e essa conversão é a deserialização
         #endregion
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;// vai pegar o o meu jso que está na string RESPOSTA e ai converter ele (deserialize) para uma lista do tipo da classe MUSICA e joar para a var musicas 
+        LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
     }
     catch (Exception ex) 
     { Console.WriteLine($"Temos um problema: {ex.Message}"); }
