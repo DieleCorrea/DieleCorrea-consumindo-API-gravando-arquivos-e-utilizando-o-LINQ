c7e9d28261dde899fb5f2e9ef3310de8b7810afa
Diele <dybs1520@gmail.com>
2023-11-11 22:05:32 -0300

    criação de um meodo onde filtro os artitas por tipo de genero musical usando o Linq

--- CsharpApiLInq/Filtros/LinqFilter.cs
+++ CsharpApiLInq/Filtros/LinqFilter.cs
@@ -7,11 +7,23 @@ internal class LinqFilter
     //não precisa dar new para usar 
     public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas)
     {
-        var todosOsGeneosMusicais = musicas.Select(generos => generos.Genero).Distinct().ToList();  //pegos a minha lista musica e seleciono somente o genero onde mostro de modo distindo transformando em uma lista . Distinct = se aparecer mais de uma vez o mesmo tipo de genero, ele mostra so um
+        var todosOsGeneosMusicais = musicas.Select(generos => generos.Genero).Distinct().ToList();  
+        //pegos a minha lista musica e seleciono somente o genero onde mostro de modo distindo
+        //transformando em uma lista . Distinct = se aparecer mais de uma vez o mesmo tipo de genero,
+        //ele mostra so um
+       
         foreach (var genero in todosOsGeneosMusicais)
         {
             Console.WriteLine($"- {genero}");
         }
+    }
+    public static void FiltrarArtistasPorGeneroMusical(List<Musica> musicas, string genero)
+    {
+        var artistasPorGeneroMusical = musicas.Where(musica => musica.Genero!.Contains(genero))
+            .Select(musica => musica.Artista).Distinct().ToList();
+        Console.WriteLine($"Exibindo os artstas por gênero musical >>> {genero}");
+        foreach( var artista  in artistasPorGeneroMusical) 
+        { Console.WriteLine($"-{artista}"); }
     }
 
 }
--- CsharpApiLInq/Program.cs
+++ CsharpApiLInq/Program.cs
@@ -5,21 +5,23 @@ using System.Text.Json;
 //recurso para fazer requisições, não usamos o ":" pois vamos usar esse recurso so dentro do using //quando a chave fecha, liberamos o recurso objeto client 
 using (HttpClient client = new HttpClient())
 {
-    
-    try 
-    { 
-    string resposta =await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-    Console.WriteLine(resposta);
+
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta);
         #region deserialização
         //Preciso ACESSAR as propriedades que estão na classe Musica,
         //mas nesse caso estao em json e para isso eu faço uma conversão de json para
         //C# e essa conversão é a deserialização
         #endregion
         var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;// vai pegar o o meu jso que está na string RESPOSTA e ai converter ele (deserialize) para uma lista do tipo da classe MUSICA e joar para a var musicas 
-        LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        LinqFilter.FiltrarArtistasPorGeneroMusical(musicas, "pop");
     }
-    catch (Exception ex) 
-    { Console.WriteLine($"Temos um problema: {ex.Message}");
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
         //pegar o json para apontar para a classe musica 
     }
 }
