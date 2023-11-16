8013641792e6b688873e280f2693f9ca05d8102f
Diele <dybs1520@gmail.com>
2023-11-16 00:15:04 -0300

    CRIAND UM ARQUIVO DO TIPO JSON

--- CsharpApiLInq/Modelos/MusicasPreferidas.cs
+++ CsharpApiLInq/Modelos/MusicasPreferidas.cs
@@ -1,4 +1,5 @@
-﻿namespace CsharpApiLInq.Modelos;
+﻿using System.Text.Json;
+namespace CsharpApiLInq.Modelos;
 /*Essa classe estou criando pois quero transformar as informações que tenho no meu arquivo c# em json
   */
 internal class MusicasPreferidas
@@ -8,8 +9,8 @@ internal class MusicasPreferidas
 
     public MusicasPreferidas(string? nome)
     {
-        Nome = nome;
-        ListaDeMusicasFavoritas = new List<Musica>() ;
+       Nome = nome;
+       ListaDeMusicasFavoritas = new List<Musica>();//inicia a lista como se ela estivesse vazia, pois quando chamar o metodo exibir ele vai pegar as prop da classe musca e criar uma nova lista através do metodo adicionar musca. É PRECISO FAZER ESSE NEW PARA NÃO DAR ERRO DE NULLREFERENCE 
 
     }
     public void AdicionarMusicasFavoritas(Musica musica)
@@ -18,10 +19,35 @@ internal class MusicasPreferidas
     }
     public void ExibirMusicasPreferidas()
     {
-        Console.WriteLine($"essas são as músicas favoritas de -> {Nome}");
+        Console.WriteLine($"Essas são as músicas favoritas de -> {Nome}");
         foreach(var musica in ListaDeMusicasFavoritas)
         {
-            Console.WriteLine($"= {musica.Nome} de {musica.Artista}");
+            Console.WriteLine($"- {musica.Nome} de {musica.Artista}");
+            
         }
+        Console.WriteLine(" ");
     }
+    public void GerarArquivoJson()
+    {
+        //SEREALIZAR A LISTA CRIADA PARA UMA STRING JSON
+        string json = JsonSerializer.Serialize(new 
+        #region// Esse new é um objeto sem tipo, ou seja, ele é um anonimo, ideal para essa situação, que to criando estrutura temporária 
+        #endregion
+        {
+            nome = Nome,
+            musicas = ListaDeMusicasFavoritas
+        }) ;
+
+        //DEFINIR QUAL VAI SER O NOME DO ARQUIVO QUE O JSON VAI SER SALVO 
+        string nomeDoArquivo = $"musicas-favoritas-{Nome}.json";
+
+        //ESCREVER A STRING JSON EM UM ARQUIVO #classe File é usada para escrever essa string em um arquivo.
+        File.WriteAllText(nomeDoArquivo, json) ;
+
+        //MOSTRA ONDE ESTÁ ESSE ARQUIVO 
+        Console.WriteLine($"O arquivo json foi criado com sucesso!{Path.GetFullPath(nomeDoArquivo)}");
+
+        //Para ver o arquivo posso abrir o vs code 
+    }
+
 }
