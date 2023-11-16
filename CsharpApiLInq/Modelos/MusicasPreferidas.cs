47abdb483eaca180821098b635d1f7856a86a073
Diele <dybs1520@gmail.com>
2023-11-15 21:36:48 -0300

    criado classe transformar as informações que tenho no meu arquivo c# em json

---
+++ CsharpApiLInq/Modelos/MusicasPreferidas.cs
@@ -0,0 +1,27 @@
+﻿namespace CsharpApiLInq.Modelos;
+/*Essa classe estou criando pois quero transformar as informações que tenho no meu arquivo c# em json
+  */
+internal class MusicasPreferidas
+{
+    public  string? Nome { get; set; }
+    public List<Musica> ListaDeMusicasFavoritas { get; }
+
+    public MusicasPreferidas(string? nome)
+    {
+        Nome = nome;
+        ListaDeMusicasFavoritas = new List<Musica>() ;
+
+    }
+    public void AdicionarMusicasFavoritas(Musica musica)
+    {
+        ListaDeMusicasFavoritas.Add(musica);
+    }
+    public void ExibirMusicasPreferidas()
+    {
+        Console.WriteLine($"essas são as músicas favoritas de -> {Nome}");
+        foreach(var musica in ListaDeMusicasFavoritas)
+        {
+            Console.WriteLine($"= {musica.Nome} de {musica.Artista}");
+        }
+    }
+}
