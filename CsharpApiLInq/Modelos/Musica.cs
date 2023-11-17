eaf5ba76877da956bdd190fdc9be90eb8a723337
DieleCorrea <dybs1520@gmail.com>
2023-11-17 11:08:21 -0300

    inseri nova propriedade do tipo tonalidade e fiz com que ela se transformasse em uma string através de uma lista.

--- CsharpApiLInq/Modelos/Musica.cs
+++ CsharpApiLInq/Modelos/Musica.cs
@@ -5,6 +5,7 @@ namespace CsharpApiLInq.Modelos;
 
 internal class Musica
 {
+    private List<string> Tonalidades = new List<string> { "C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B" };
     [JsonPropertyName("song")]//atributo que representa um metadado especifico para a propiedade Nome 
     public string Nome { get; set; }
     [JsonPropertyName("artist")]
@@ -12,17 +13,34 @@ internal class Musica
     [JsonPropertyName("duration_ms")]
     public int Duracao { get; set; }
     [JsonPropertyName("genre")]
-    public string? Genero { get; set;}// o "?" é que pode ser nula 
+    public string? Genero { get; set; }// o "?" é que pode ser nula 
     [JsonPropertyName("year")]
     public string AnoString { get; set; }
-    //public int Ano => int.Parse(AnoString);
+    public int Ano => int.Parse(AnoString);
+    [JsonPropertyName("key")]
+    public int Key { get; set; }
+    public string Tonalidade
+    {
+        get
+        {
+            return Tonalidades[Key];
+        }
+    }
+
 
     public void ExibirDetalhesDamusica()
     {
         Console.WriteLine($"Artista: {Artista}");
         Console.WriteLine($"Musica: {Nome}");
-        Console.WriteLine($"Duração em segundos: {Duracao /1000}");
+        Console.WriteLine($"Duração em segundos: {Duracao / 1000}");
         Console.WriteLine($"Genero musical: {Genero}");
         Console.WriteLine($"Ano da musica: {AnoString}");
+        Console.WriteLine($"Tom da música : {Tonalidade}");
     }
+    /*O desafio é o seguinte: você deverá criar um método que exiba o nome de todas as 
+     * músicas que estejam na tonalidade de C#, ou seja, dó sustenido.
+
+    Lembrando que, na nossa lista do GitHub, a tonalidade que representa o C# corresponde ao índice 1. 
+    Então, a ideia é que você busque todas as músicas com índice 1 e exiba apenas elas.*/
+   
 }
